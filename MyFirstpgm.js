48666543aa6b3c64b1138f153bb6860341377a38
miltonjesurajan <90078582+miltonjesurajan@users.noreply.github.com>
2026-06-03 20:45:38 +0530

    5th commit
    Update MyFirstpgm.js

--- MyFirstpgm.js
+++ MyFirstpgm.js
@@ -8,7 +8,15 @@ function add(a, b)
 {
     console.log(a+b)
 }      
+function sub(d,e){
+    console.log(d-e)
+}
+function div(k,l){
+    console.log(k/l)
+}
 
 
 printString("Raj","S")
 add(10,25)
+sub(25,5)
+div(10,5)
