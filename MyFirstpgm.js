58b9343d0b4864b62101f0da3b4545692b8f2c6a
veelinktech <104140334+veelinktech@users.noreply.github.com>
2026-06-03 20:30:21 +0530

    2nd commit
    Refactor MyFirstpgm.js to use functions for logging

--- MyFirstpgm.js
+++ MyFirstpgm.js
@@ -1,4 +1,14 @@
-console.log("Hello JS")
-console.log("Playwright")
-console.log(5+5)
-            
+function printSring()
+{
+    console.log("Hello JS")
+    console.log("Playwright")
+}
+
+function add()
+{
+    console.log(5+5)
+}      
+
+
+printString()
+add()
